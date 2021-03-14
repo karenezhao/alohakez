d7840064b4d6f80969b8e215df4d5b4fb9541b11
karenezhao <karenezhao@ucsb.edu>
2021-03-14 15:12:50 -0700

    modify cce_sst

--- data-raw/cce_sst.R
+++ data-raw/cce_sst.R
@@ -1,4 +1,4 @@
-## code to prepare `cce_watertemp` dataset goes here
+## code to prepare `cce_sst` dataset goes here
 
 # Attach packages
 library(usethis)
@@ -8,11 +8,11 @@ library(metajam)
 cce_url <- "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-cce.15.4&entityid=411c8b8769c4578be6702ee44bdb712e"
 
 # Download the data package with metajam
-cce_download <- download_d1_data(data_url = cce_url, path = tempdir(), dir_name="cce_watertemp")
+cce_download <- download_d1_data(data_url = cce_url, path = tempdir(), dir_name="cce_sst")
 
 # Read in data
 cce_files <- read_d1_files(cce_download)
-cce_watertemp <- cce_files$data %>%
+cce_sst <- cce_files$data %>%
   clean_names()
 
-usethis::use_data(cce_watertemp, overwrite = TRUE)
+usethis::use_data(cce_sst, overwrite = TRUE)
