1a6c1ff5560162b96e5881cd29f8a8c9827c129b
karenezhao <karenezhao@ucsb.edu>
2021-05-24 17:43:46 -0700

    add pie_marsh data

---
+++ R/pie_marsh_doc.R
@@ -0,0 +1,13 @@
+#' Aboveground plant biomass and density in control and fertilized plots in a Spartina alterniflora-dominated marsh, Rowley River, Plum Island Ecosystem LTER, MA.
+#'
+#' Aboveground plant biomass and density is determined non-destructively during the growing season at a Spartina alterniflora-dominated salt marsh on the Rowley River within the Plum Island Ecosystems (PIE) LTER site.
+#'
+#' @format A tibble with 1,404 rows and 9 variables:
+#' \describe{
+#'   \item{treatment}{a string denoting the treatment applied to the plot; N: fertlized with nitrogen only, P: fertilized with phosphorus only, NP: fertilized with nitrogen and phosphorus, C: control (not fertilized)}
+#'   \item{biomass}{biomass dry weight}
+#'   \item{density}{plant density}
+#'   }
+#' @source {Morris, J. 2020. Aboveground plant biomass and density in control and fertilized plots in a Spartina alterniflora-dominated marsh, Rowley River, Plum Island Ecosystem LTER, MA. ver 2. Environmental Data Initiative.}
+#' \url{https://doi.org/10.6073/pasta/d2d045ed569f51d1bf23637b42cf8ea6}
+"pie_marsh"
